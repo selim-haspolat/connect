3f0c1ec8824689110eb328029b4e84731d513427
selim-haspolat <118964736+selim-haspolat@users.noreply.github.com>
2023-03-06 19:20:55 +0300

    userprofile update

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const UserCard = ({userName,email,profileImage,posts}) => {
+const UserCard = ({userName,id,email,profileImage,posts}) => {
 
   const {userName:localUserName} = JSON.parse(localStorage.getItem('userData') )
+  const navigate = useNavigate();
+  const displayUser = () => {
+    navigate(`/${id}`)
+  };
 
   return (
     <li className="py-3 sm:py-4">
     <div className="flex items-center space-x-4">
       <div className="flex-shrink-0">
-        <img className="w-10 h-10 rounded-full" src={profileImage} alt={userName} />
+        <img onClick={displayUser} className="w-10 h-10 rounded-full cursor-pointer" src={profileImage} alt={userName} />
       </div>
       <div className="flex-1 min-w-0">
         <p className={`text-sm font-medium  truncate text-white  ${localUserName === userName && '!text-blue-500'}`}>
